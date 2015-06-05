01e443306f261a17172703ca4dc448f3a4cf33d3
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2015-06-05 18:52:59 +0200

    add the socket to read RDF

--- middleware.pl
+++ middleware.pl
@@ -10,11 +10,18 @@ use AtteanX::Store::Memory;
 use Attean::RDF;
 
 
+my ($address, $port) = @ARGV;
+
+my $fh = IO::Socket::INET->new(PeerPort  => $port,
+										 PeerAddr  => $address
+										 Proto     => 'udp');
+
+
 # TODO some kind of socket that connects to the arduino and loops reading from there.
 
 my $model = Attean::TripleModel->new(store => AtteanX::Store::Memory->new);
 my $parser	= AtteanX::Parser::Turtle->new(handler => translate);
-while () {
+while (1) {
   $parser->parse_cb_from_io($fh, 'http://example.org/');
 }
 
